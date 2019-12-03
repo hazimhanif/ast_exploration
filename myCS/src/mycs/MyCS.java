f01222071cddb6d080083af71a67133a720f1f16
Mohamad Md Hanif <mhm1718@garonne.doc.ic.ac.uk>
2019-12-03 18:00:42 +0000

    Updated parser

--- myCS/src/mycs/MyCS.java
+++ myCS/src/mycs/MyCS.java
@@ -20,7 +20,12 @@ import java.util.ArrayList;
 public class MyCS {
 
     static final String separator = "\t";
-    
+    static ArrayList<Integer> level = new ArrayList();
+    static ArrayList<Object> in = new ArrayList();
+    static ArrayList<Integer> inlvl = new ArrayList();
+    static ArrayList<Object> temp = new ArrayList();
+    static ArrayList<Object> temp2 = new ArrayList();
+            
     public static void main(String[] args) throws IOException
 	{
 	    CodeTree codeTree = new CodeTree();
@@ -49,55 +54,79 @@ public class MyCS {
 	    } catch (Exception e) {
 		e.printStackTrace();
 	    }
-            
-            
-            ArrayList<Object> in = new ArrayList();
-            ArrayList<Integer> inlvl = new ArrayList();
-            ArrayList<Object> out = new ArrayList();
-            ArrayList<Object> temp = new ArrayList();
-            ArrayList<Object> temp2 = new ArrayList();
+
             
             in.add("=");
             in.add("x");
             in.add("2");
+            in.add("z");
+            in.add("y");
+            in.add("k");
+            in.add("q");
+            in.add("a");
             inlvl.add(10);
             inlvl.add(11);
             inlvl.add(11);
+            inlvl.add(11);
+            inlvl.add(12);
+            inlvl.add(12);
+            inlvl.add(12);
+            inlvl.add(13);
             
             System.out.println(in);
             System.out.println(inlvl);
-            int head =0;
-            
-            for(int i=1;i<in.size();i++){
-                if(i==1){
-                    // first child
-                    head = inlvl.get(i-1);
-                    temp.add(in.get(i));
-                    in.set(i, temp.clone());
-                    temp.clear();
-                }else if(inlvl.get(i)>inlvl.get(i-1)){
-                    // child
-                    
+            System.out.println("");
 
-                }else{
-                    // same level
-                    temp.add(in.get(i));
-                    in.set(i, temp.clone());
-                    temp.clear();
-                }
-            }
+            level.add(inlvl.get(0));
+            makeChild();
             
-//                    temp.add(in.get(i));
-//                    in.remove(i);
-//                    temp2 = (ArrayList<Object>)in.get(i-1);
-//                    temp2.add(temp.get(0));
-//                    in.set(i-1, temp2.clone());
-//                    temp.clear();
-//                    temp2.clear();
-            
+ 
             System.out.println(in);
+            System.out.println(level);
 	}
-	    
+    
+    public static void makeChild(){
+        
+        for(int i=1;i<in.size();i++){
+             if(i==1){
+                 // first child
+                 temp.add(in.get(i));
+                 in.set(i, temp.clone());
+                 temp.clear();
+                 level.add(inlvl.get(i));
+             }else if(inlvl.get(i)>inlvl.get(i-1)){
+
+                  //child
+                 int j=1;
+                
+                if(inlvl.get(i)!=level.get(level.size()-1))
+                      level.add(inlvl.get(i));
+                 
+                 while(!(((ArrayList<Object>)in.get(j)).size()<3)){
+                     j++;
+                 }
+
+                 temp.add(in.get(i));
+                 in.remove(i);
+                 temp2 = (ArrayList<Object>)in.get(j);
+                 temp2.add(temp.clone());
+                 in.set(j, temp2.clone());
+                 
+                 temp.clear();
+                 temp2.clear();
+                 i--;
+             }else{
+                  //same level
+                  
+                 temp.add(in.get(i));
+                 in.set(i, temp.clone());
+                 temp.clear();
+             }
+         }
+            
+
+    }
+    
     private static String parseCommandLine(String[] args) throws Exception
 	{
 	    if(args.length != 1){
